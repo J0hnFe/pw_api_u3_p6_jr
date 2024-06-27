dc58faec959a411c693f8933fd36587ef9fd2a2e
John <johnfe@outlook.es>
2024-06-26 19:22:22 -0500

    commit inicial

---
+++ src/main/java/com/example/pw/pw_api_u3_p6_jr/PwApiU3P6JrApplication.java
@@ -0,0 +1,15 @@
+package com.example.pw.pw_api_u3_p6_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwApiU3P6JrApplication {
+
+	// test
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwApiU3P6JrApplication.class, args);
+	}
+
+}
